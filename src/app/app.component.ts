393a62870817781b69ac3ec9c77a1083cc02e95b
Elliot Lehman <elliot.lehman@protonmail.com>
2024-03-16 08:24:28 -0600

    angular learning bump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 @Component({
-  standalone: true,
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
   template: `
-        <main>
-          <header class="brand-name">
-            <img class="brand-logo" src="/assets/logo.svg"
-            alt="logo" aria-hidden="true">
-        `,
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
